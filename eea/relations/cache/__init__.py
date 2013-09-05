fa0b6ca7743a685414341794caae93e72bc87af5
ichim-david <ichim.david@gmail.com>
2013-09-05 19:08:40 +0300

    Started adding cache directory with logic for future caching

---
+++ eea/relations/cache/__init__.py
@@ -0,0 +1,17 @@
+""" Caching module
+"""
+try:
+    from eea.cache import cache as eeacache
+    from eea.cache import event
+    # pyflakes
+    InvalidateCacheEvent = event.InvalidateCacheEvent
+    ramcache = eeacache
+except ImportError:
+    # Fail quiet if required cache packages are not installed in order to use
+    # this package without caching
+    from eea.relations.cache.nocache import ramcache
+
+__all__ = [
+    ramcache.__name__,
+    InvalidateCacheEvent.__name__,
+]
